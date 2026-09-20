22eb940960183f08b36cb29783a24c79fd9ec284
vectechcb-hue <vectech.cb@gmail.com>
2026-09-20 10:57:19 +0800

    加入新版 App 頁面並更新快取版本

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='cbtrade-ppt-converter-v8';
-const SHELL=['./','./index.html','./converter.html','./manifest.webmanifest','./icon.svg','./vendor/pptxgen.min.js'];
+const SHELL=['./','./index.html','./converter.html','./app.html','./manifest.webmanifest','./icon.svg','./vendor/pptxgen.min.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
